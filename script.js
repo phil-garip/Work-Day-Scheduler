9d6513a5d67abf4a209f2c03f126ca9ccaa0b3f0
Philip Garip <phil.garip@gmail.com>
2021-04-21 20:13:36 -0400

    added variables in javascript to select HTML elements

---
+++ script.js
@@ -0,0 +1,10 @@
+// DEPENDENCIES========================================
+    // current day p tag
+var currentDay = $('#currentDay');
+    // time-block div tag
+var timeBlockEl = $('#time-block');
+// STARTING DATA=======================================
+
+// FUNCTIONS===========================================
+
+// USER INTERACTIONS===================================
