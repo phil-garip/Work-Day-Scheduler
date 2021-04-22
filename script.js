74ed18d701832b1821a04cea81fe578940c43be3
Philip Garip <phil.garip@gmail.com>
2021-04-21 20:28:04 -0400

    added all the timeblock elements

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // DEPENDENCIES========================================
     // current day p tag
-var currentDay = $('#currentDay');
+var currentDayEl = $('#currentDay');
     // time-block div tag
 var timeBlockEl = $('#time-block');
+    // save button
+var saveBtn = $('#save-button');
+    // hour block
+var hourEl = $('#hour');
 // STARTING DATA=======================================
 
 // FUNCTIONS===========================================
-
+    // if time of day matches an hour element, change it's class to present
+    
 // USER INTERACTIONS===================================
