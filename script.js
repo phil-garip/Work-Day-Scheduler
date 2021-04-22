745aa68358b0784df390d29c5e9d1d84accbe985
Philip Garip <phil.garip@gmail.com>
2021-04-21 21:08:55 -0400

    commit

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ var hourEl = $('#hour');
     // if time of day matches an hour element, change it's class to present
     
 // USER INTERACTIONS===================================
+
+
+
+
+
+// USER STORY
+//when a user logs on
+//they can add tasks and events to each hour of the workday
+//and those tasks will be saved locally
+//future rows will be green
+//row that holds current hour will be red
+//rows from hours past will turn gray
