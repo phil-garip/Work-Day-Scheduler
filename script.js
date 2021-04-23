e44a2a3e45509a2b146175c566d3f29974729577
Philip Garip <phil.garip@gmail.com>
2021-04-23 18:30:13 -0400

    bug fix

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function renderTextBlocks() {
         dailyTasks.push(($(this).siblings("textArea").val()));
         localStorage.setItem("Data", dailyTasks);
        }
-
+    }
     };
     
     
