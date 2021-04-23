84fd60aa3ec82d253787dac0e12c040e77d5c0e6
Philip Garip <phil.garip@gmail.com>
2021-04-22 21:03:56 -0400

    updated hours

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // DEPENDENCIES========================================
+
+const moment = require("moment");
+
  //text block container
  var textBlockContainer = $("#text-block-container");
 
@@ -8,49 +11,50 @@
 var today = moment();
 var hourBlocks = [
     { 
-        hour: "0900"
+        hour: "09"
     },
     {
-        hour: "1000"
+        hour: "10"
     },
     {
-        hour: "1100"
+        hour: "11"
     },
     {
-        hour: "1200"
+        hour: "12"
     },
     {
-        hour: "1300"
+        hour: "13"
     },
     {
-        hour: "1400"
+        hour: "14"
     },
     {
-        hour: "1500"
+        hour: "15"
     },
     {
-        hour: "1600"
+        hour: "16"
     },
     {
-        hour: "1700"
+        hour: "17"
     }
 ];
 
 // FUNCTIONS===========================================
 // dynamically render text blocks
 function renderTextBlocks() {
-
+    // loop through objects
     for(i=0; i<hourBlocks.length; i++) {
-
+        // create elements
         var hourBlock = $('<div class="hour col-sm-1" id="hour">').text(hourBlocks[i].hour);
         var timeBlock= $('<div class="time-block row" id="time-block">');
         var textArea = $('<textarea id="user-input" class="future col-sm-10" placeholder="Add Event">');
         var button = $('<button class="saveBtn col-sm-1" id="save-button">').text("save");
-        
+        //append elements to each other and then to the page
         timeBlock.append(hourBlock, textArea, button);
         textBlockContainer.append(timeBlock);
+        //translate time using moment
+        moment(hourBlock).format()
 
-        
         
     }
 
