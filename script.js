b55033ce495aac6b7068f77de84e3ab87c83697d
Philip Garip <phil.garip@gmail.com>
2021-04-22 20:50:46 -0400

    wrote render function for hour blocks

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // STARTING DATA=======================================
     // list of text-block objects
 var today = moment();
-var textBlocks = [
+var hourBlocks = [
     { 
         hour: "0900"
     },
@@ -39,16 +39,35 @@ var textBlocks = [
 // FUNCTIONS===========================================
 // dynamically render text blocks
 function renderTextBlocks() {
-    //create timeblock div
-    var timeBlock= $('<div class="time-block row" id="time-block">');
-    var hourBlock = $('<div class="hour col-sm-1" id="hour">');
-    var textArea = $('<textarea id="user-input" class="future col-sm-10" placeholder="Add Event">');
-    var button = $('<button class="saveBtn col-sm-1" id="save-button">');
+
+    for(i=0; i<hourBlocks.length; i++) {
+
+        var hourBlock = $('<div class="hour col-sm-1" id="hour">').text(hourBlocks[i].hour);
+        var timeBlock= $('<div class="time-block row" id="time-block">');
+        var textArea = $('<textarea id="user-input" class="future col-sm-10" placeholder="Add Event">');
+        var button = $('<button class="saveBtn col-sm-1" id="save-button">').text("save");
+        
+        timeBlock.append(hourBlock, textArea, button);
+        textBlockContainer.append(timeBlock);
+
+        
+        
+    }
+
     
+    //create timeblock div
+    // var hourBlock = $('<div class="hour col-sm-1" id="hour">');
+    // var timeBlock= $('<div class="time-block row" id="time-block">');
+    // var textArea = $('<textarea id="user-input" class="future col-sm-10" placeholder="Add Event">');
+    // var button = $('<button class="saveBtn col-sm-1" id="save-button">');
+    
+    
+
     //add components to timeBlock
-    timeBlock.append(hourBlock, textArea, button);
-    //add timeblock to page
-    textBlockContainer.append(timeBlock);
+    // timeBlock.append(textArea, button. hourBlock);
+
+    // //add timeblock to page
+    // textBlockContainer.append(timeBlock);
 }
 
 renderTextBlocks();
